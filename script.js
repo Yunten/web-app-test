18be23c087fe05573a4e801af7e213d10a4fe61b
Yunten <kreeg1@yandex.ru>
2024-08-30 12:52:28 +0300

    up

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     ${visit.name} - ${visit.cost}₽ в ${visit.startTime}
                     <div class="geoloc">${visit.geoloc}</div>
                 </span>
-                ${visit.lat ? `<img src="map_icon.png" class="map-icon" onclick="javascript:Telegram.WebApp.openLink('https://yandex.ru/maps/?ll=${visit.lng},${visit.lat}&z=19&l=map');" alt="Show on Map">`:''}
+                ${visit.lat ? `<img src="map_icon.png" class="map-icon" onclick="javascript:Telegram.WebApp.openLink('https://yandex.ru/maps/?ll=${visit.lng},${visit.lat}&z=19&l=map');" alt="Открыть карту">`:''}
             </li>
         `).join('');
         
